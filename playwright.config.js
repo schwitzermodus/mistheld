30c74171f201e85c58a4ea75be266c81b869141c
schwitzermodus <sascha.muelhausen@gmail.com>
2026-05-03 22:25:53 +0200

    ci: add playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 15000,
+  retries: 0,
+  reporter: 'list',
+  use: {
+    baseURL: 'http://localhost:3000',
+    ignoreHTTPSErrors: true,
+  },
+  webServer: {
+    command: 'npx serve . -p 3000 --no-clipboard',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
