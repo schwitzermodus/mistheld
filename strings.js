e9c0352a9bf8526b27626cf9c0dd9d99497c1d2e
schwitzermodus <sascha.muelhausen@gmail.com>
2026-06-07 21:09:08 +0200

    Delete strings.js

--- strings.js
+++
@@ -1,9 +0,0 @@
-/*
-  HINWEIS: Diese Datei wird nicht mehr geladen.
-
-  Alle Texte befinden sich jetzt direkt in index.html
-  als inline <script>-Block am Ende der Datei.
-
-  Einfach index.html auf GitHub öffnen, zum Ende
-  scrollen, den STRINGS-Block finden und dort bearbeiten.
-*/
